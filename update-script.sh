0ddf34e08dcd7b1eb368fe21dc59b694e20f9c6a
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-08-07 05:23:04 +0700

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/Mahfud2128/mini/bckp/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh && rm -rf set-br.sh
+wget -O set-br.sh https://github.com/Mahfud2128/mini/blob/main/bckp/set-br.sh?raw=true && chmod +x set-br.sh && ./set-br.sh && rm -rf set-br.sh
