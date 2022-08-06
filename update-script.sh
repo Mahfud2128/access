e4995ad129dbb002211e884b74eb1aa2b4aa0713
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-08-07 05:27:10 +0700

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 wget -O set-br.sh https://github.com/Mahfud2128/mini/blob/main/bckp/set-br.sh?raw=true && chmod +x set-br.sh && ./set-br.sh && rm -rf set-br.sh
+serverV=$( curl -sS https://raw.githubusercontent.com/Mahfud2128/access/main/versi  )
+echo $serverV > /opt/.ver
