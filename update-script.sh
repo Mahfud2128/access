61266dc4c18b7e460c37fef3bf08c199800eede2
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-08-12 15:38:51 +0700

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -2,6 +2,7 @@
 wget -O set-br.sh https://github.com/Mahfud2128/mini/blob/main/bckp/set-br.sh?raw=true && chmod +x set-br.sh && ./set-br.sh && rm -rf set-br.sh
 wget -O /usr/bin/webmin https://raw.githubusercontent.com/Mahfud2128/mini/main/webmin.sh && chmod +x /usr/bin/webmin
 wget -O /usr/bin/cek-ws https://raw.githubusercontent.com/Mahfud2128/mini/main/xray/cek-ws.sh && chmod +x /usr/bin/cek-ws
+wget -O /usr/bin/menu https://raw.githubusercontent.com/Mahfud2128/mini/main/menu.sh && chmod +x /usr/bin/menu
 serverV=$( curl -sS https://raw.githubusercontent.com/Mahfud2128/access/main/versi  )
 echo $serverV > /opt/.ver
 echo "Update Selesai"
